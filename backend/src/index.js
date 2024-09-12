a71c3f1c80f13fcb6557b479cb47da1fc531383b
swafvanSFK <safuswafvan1@gmail.com>
2024-09-12 10:20:17 +0530

    Changed cors option

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,9 +11,8 @@ const app = express();
 
 // Set up cors origin
 //http://localhost:5173
-
 const corsOptions = {
-  origin : ['https://mensfashionapp.onrender.com'],
+  origin : ['https://mensfashionapp-1.onrender.com'],
   credentials: true,
   methods: 'GET,POST,PUT,DELETE',
 }
