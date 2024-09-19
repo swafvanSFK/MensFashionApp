0c9a1d3ee1872ddddc93de2988841506f4d2eaa8
swafvanSFK <safuswafvan1@gmail.com>
2024-09-19 11:07:59 +0530

    Remove track order option

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,9 +10,9 @@ import cookieParser from 'cookie-parser'
 const app = express();
 
 // Set up cors origin
-//https://mensfashionapp-1.onrender.com
+//http://localhost:5173
 const corsOptions = {
-  origin : ['http://localhost:5173'],
+  origin : ['https://mensfashionapp-1.onrender.com'],
   credentials: true,
   methods: 'GET,POST,PUT,DELETE',
 }
